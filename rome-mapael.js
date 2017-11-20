46583a3b37150957129f08213fe7afbd19a0c85c
RosenDangov <ros95@mail.bg>
2017-11-20 19:22:05 +0000

    Switched Raphael with Mapael

---
+++ rome-mapael.js
@@ -0,0 +1,14 @@
+
+$(function(){$(".container").mapael({
+    map : {
+        name : "resources-mapael"
+    }});	
+});
+
+
+var items = startElem.getElementsByTagName("*");
+for(var i = items.length; i--;){
+	if(i.css('.mapTooltip') == null){
+		console.log("lul");
+	}
+}
